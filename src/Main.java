516464cb213facf05936487a6793d0572091fb82
Przemysław Kozłowski <s35511@pjwstk.edu.pl>
2026-03-11 10:49:09 +0100

    Dodanie pomocniczej informacji o aplikacji

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main{
     public static void main(String[] args){
-        System.out.println("Przykladowy kod na przykladowym repozytorium")
+        System.out.println("Przykladowy kod na przykladowym repozytorium");
+        System.out.println("wiadomosc pomocnicza moze w czyms komus pomoze");
     }
 }
